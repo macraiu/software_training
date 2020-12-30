5d18499d200cef6d3b0121d90efb0b7a73cab812
abramovich <macraiu@yahoo.it>
2020-12-30 18:33:33 +0000

    other leetcodE

--- leetcode/1512_number_of_good_pairs.py
+++ leetcode/1512_number_of_good_pairs.py
@@ -1,3 +1,45 @@
+""" 
+Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+ """
+import math 
+
 def numIdenticalPairs(nums):
+    d = {}
+    for i in nums:
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] += 1
     
+    good_couples = 0
+    for i in d:
+        if d[i] >= 2:
+            #fact(n)/fact(r)*fact(n-r)
+            #fact(n)/(2*fact(n-2)
+            good_couples += math.factorial(d[i])//(2*math.factorial(d[i] - 2))
+
+    return good_couples
+
+print(numIdenticalPairs([1, 2,3,3, 1, 1]))
         
--- leetcode/405_Convert_a_Number_to_Hexadecimal.py
+++ leetcode/405_Convert_a_Number_to_Hexadecimal.py
@@ -1,8 +1,47 @@
+""" 
+Given an integer, write an algorithm to convert it to hexadecimal. For negative integer, two’s complement method is used.
+
+Note:
+
+All letters in hexadecimal (a-f) must be in lowercase.
+The hexadecimal string must not contain extra leading 0s. If the number is zero, it is represented by a single zero character '0'; otherwise, the first character in the hexadecimal string will not be the zero character.
+The given number is guaranteed to fit within the range of a 32-bit signed integer.
+You must not use any method provided by the library which converts/formats the number to hex directly.
+Example 1:
+
+Input:
+26
+
+Output:
+"1a"
+Example 2:
+
+Input:
+-1
+
+Output:
+"ffffffff"
+ """
+
+
 def toHex(num):
-    d = {10:'a', 11:'b', 12:'c', 13:'d', 14:'e', 15:'f'}
-    print(num/16)
 
-print(toHex(500))
+    d = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9', 10:'a', 11:'b', 12:'c', 13:'d', 14:'e', 15:'f'}
+    h = []
 
-print(hex(500))
-print(bin(160))
+    if num == 0:
+        return '0'
+
+    if num < 0:
+        num = 2**32 + num
+
+    while num > 0:
+        h.append(d[num % 16])
+        num = num // 16
+    
+    h.reverse()
+    return "".join(h)
+
+num = -17
+print(toHex(num))
+print(hex(num))
