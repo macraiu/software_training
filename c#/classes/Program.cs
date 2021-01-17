14234fff981dc7ebe85d1852838a585ded4cefa9
AbramovichCaesar <macraiu@yahoo.it>
2021-01-17 06:07:28 +0000

    new vs

---
+++ c#/classes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace classes
+{
+
+    public class Person{
+        
+        public string Name;
+        public int Age;
+
+        public Person(string name){
+            this.Name = name;
+        }
+
+        public Person(string name, int age){
+            this.Name = name;
+            this.Age = age;
+        }
+
+
+    }
+
+    public class 
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person =  new Person("John Smith", 23);
+            Console.WriteLine("Name: {0} Age: {1}", person.Name, person.Age);
+
+        }
+    }
+}
